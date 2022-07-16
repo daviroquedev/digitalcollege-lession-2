08250cbd1ff4cbcc486721715cf026894db13075
daviroquedev <sowdavi@gmail.com>
2022-07-16 16:41:16 -0300

    adicionando codigo sidebar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './main.css'
 import { Modal } from './modal'
+import { Sidebar } from './sidebar'
 
+const sidebarHandler = Sidebar()
 const modalHandler = Modal()
 
 modalHandler.init()
 // modalHandler.openModal()
+
+sidebarHandler.init()
---
+++ src/sidebar.js
@@ -0,0 +1,14 @@
+export function Sidebar(){
+    function init(){
+        const sideBarOpen = document.querySelector('[data-open-sidebar]')
+        
+    }
+
+    return{
+        init
+    }
+
+}
+
+Sidebar()
+
